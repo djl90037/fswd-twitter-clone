dff4568bdbb5b85e9ab62a6b0e99b523dac8058f
daniel <djl90037@gmail.com>
2022-11-08 20:59:57 -0600

    transfer work between pc's added fetchHelper

--- app/javascript/src/homeComponents/home.jsx
+++ app/javascript/src/homeComponents/home.jsx
@@ -10,10 +10,11 @@ const Home = (props) => {
   return (
     <React.Fragment>
       <HomeNav />
-      <div className="container mt-5">
+      <div className="container main">
         <div className="row">
-          <div className="welcome-text col-7">
-            <p><strong>Welcome to Twitter</strong></p>
+          <div className="welcome col-7">
+            <p id="welcome-title"><strong>Welcome to Twitter</strong></p>
+            <p id="welcome-text">Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum </p>
           </div>
           <div className="log-in col-5">
             <Login />
--- app/javascript/src/homeComponents/signup.jsx
+++ app/javascript/src/homeComponents/signup.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { safeCredentials, handleErrors } from '../utils/fetchHelper'
 
 class Signup extends Component {
   constructor(props) {
@@ -10,14 +11,15 @@ class Signup extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    // this.createSession = this.createSession.bind(this);
+    this.createSession = this.createSession.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ 
-      email: '', 
-      username: '', 
-      password: '' });
+    this.setState({
+      email: '',
+      username: '',
+      password: ''
+    });
   }
 
   handleChange = (event) => {
@@ -32,30 +34,77 @@ class Signup extends Component {
       case "password":
         this.setState({ password: value });
         break;
-    } 
+    }
   }
 
-  handleSubmit = (evemt) => {
+  handleSubmit = (event) => {
     event.preventDefault();
     const { email, username, password } = this.state;
 
-    fetch('/api/users')
+    fetch('/api/users', safeCredentials({
+      method: 'POST',
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+          email,
+        }
+      })
+    }))
+      .then(handleErrors)
+      .then(data => {
+        if (data.success) {
+          console.log('success');
+          this.createSession();
+        }
+        else {
+          throw new Error('Invalid username or password');
+        }
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+
+  createSession = () => {
+    const { username, password } = this.state;
+    fetch('/api/sessions', safeCredentials({
+      method: 'POST',
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
+        }
+      })
+    }))
+      .then(handleErrors)
+      .then(data => {
+        if (data.success) {
+          window.location.href = '/feeds';
+        }
+        else {
+          throw new Error('Invalid username or password');
+        }
+      })
+      .catch(error => {
+        alert(error);
+      });
   }
 
   render() {
     return (
       <React.Fragment>
-        <div className="card p-2 mt-5 border border-primary">
+        <div className="card signup-card p-2 border border-primary">
           <div className="sign-in-title card-title"><h4>Sign up here</h4></div>
           <form id="signup-form" onSubmit={this.handleSubmit}>
             <div className="form-group">
-              <input onChange={this.handleChange} type="text" name="username" value={this.username} className="form-control username mb-2 border border-primary" placeholder="username"/>
+              <input onChange={this.handleChange} type="text" name="username" value={this.username} className="form-control username mb-2 border border-primary" placeholder="username" />
             </div>
             <div className="form-group col-xs-8">
-              <input onChange={this.handleChange} type="email" name="email" value={this.email} className="form-control email my-2 border border-primary" placeholder="Email"/>
+              <input onChange={this.handleChange} type="email" name="email" value={this.email} className="form-control email my-2 border border-primary" placeholder="Email" />
             </div>
             <div className="form-group col-xs-8">
-              <input onChange={this.handleChange} type="password" name="password" value={this.password} className="form-control password my-2 border border-primary" placeholder="Password"/>
+              <input onChange={this.handleChange} type="password" name="password" value={this.password} className="form-control password my-2 border border-primary" placeholder="Password" />
             </div>
             <button id="sign-in-btn" className="btn btn-default btn-primary col-xs-3">Sign up</button>
           </form>
